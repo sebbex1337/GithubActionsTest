83bbcbfd54e45ab53771703fc29fb788b2dec6ce
sebbex1337 <sefort@hotmail.dk>
2024-02-06 09:53:43 +0100

    init

---
+++ src/test/java/org/example/githubactionstest/GithubActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
